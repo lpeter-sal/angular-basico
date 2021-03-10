b990877892435140be22dfaec5857691b4360d01
Pedro Salvador <lp.salvador@info.uas.edu.mx>
2021-03-10 10:12:35 -0700

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
+  /*Se importan todos los modulos de la
+  aplicacion para que se pueda trabajar
+  por partes sin necesidad de tener
+  todo en un solo modulo*/
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
